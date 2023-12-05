6f31c9d02663ce73c7ebe613750abd3b365cfa62
kancharlapurnaadithya <147489885+kancharlapurnaadithya@users.noreply.github.com>
2023-12-05 13:24:28 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d",&a);     b=a*4;     if(b<=1000)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Tickets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    b=a*4;
+    if(b<=1000)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
