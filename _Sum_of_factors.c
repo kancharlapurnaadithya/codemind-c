164af7453d4aae4f231f336c496e5db2fa3af805
kancharlapurnaadithya <147489885+kancharlapurnaadithya@users.noreply.github.com>
2023-12-12 21:01:36 +0530

    #include<stdio.h> int main() {     int a,i,s=0;     scanf("%d",&a);     for(i=1;i<a;i++)     {         if(a%i==0)         {            s=s+i;//{           // printf("%d",s);            //}         }         //printf("%d",s);     }{            printf("%d",s);            } }

---
+++ _Sum_of_factors.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,s=0;
+    scanf("%d",&a);
+    for(i=1;i<a;i++)
+    {
+        if(a%i==0)
+        {
+           s=s+i;//{
+          // printf("%d",s);
+           //}
+        }
+        //printf("%d",s);
+    }{
+           printf("%d",s);
+           }
+}
